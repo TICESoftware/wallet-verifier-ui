0240d660a8e2bf5c89b7cb98887b60d2bd246e4c
Tim Schauder <temscha@live.com>
2024-07-03 16:13:51 +0200

    Add tailwind and daisyUI

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-
 // @ts-ignore
 export default defineNuxtConfig({
   ssr: false,
   modules: [
     '@nuxt/eslint',
     '@pinia/nuxt',
+    '@nuxtjs/tailwindcss',
   ],
   devtools: { enabled: true },
   runtimeConfig: {
     public: {
       apiUrl: 'http://localhost:8080',
       version: 'dev',
-    }
+    },
   }
 })
