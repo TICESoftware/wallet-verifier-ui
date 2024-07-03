e31cb9f6d930d4ba37af0aa18027bb90a1787f69
Tim Schauder <temscha@live.com>
2024-07-03 18:15:32 +0200

    Add wallet theme

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div data-theme="wallettheme">
+      <slot />
+  </div>
+</template>
+
+<style scoped></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,19 @@ module.exports = {
     plugins: [
         daisyui,
     ],
+    daisyui: {
+        themes: [
+            {
+                wallettheme: {
+                    "primary": "#a991f7",
+                    "secondary": "#a991f7",
+                    "accent": "#a991f7",
+                    "neutral": "#a991f7",
+                    "base-100": "#F2F7FC",
+                },
+            },
+        ],
+    },
     content: [
         "./components/**/*.{js,vue,ts}",
         "./layouts/**/*.vue",
