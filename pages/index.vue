4bd24fef851684ef7262a604babe552f28750c71
Tim Schauder <temscha@live.com>
2024-07-12 16:42:20 +0200

    Add text to page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,11 @@
-<script setup lang="ts">
-  // TODO...
-</script>
 
 <template>
-  <div>
-    // TODO...
+  <div class="flex flex-col justify-center items-center">
+    <div class="text-xl lg:text-3xl font-bold text-accent m-6">
+      <h1>Privacy by Design</h1>
+    </div>
+    <button class="btn btn-primary">Jetzt testen</button>
   </div>
 </template>
+<script setup lang="ts">
+</script>
