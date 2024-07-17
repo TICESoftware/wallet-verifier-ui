c6b493f7814b2e8aba853b361a406f286a9d6420
Tim Schauder <temscha@live.com>
2024-07-17 17:35:45 +0200

    Add info accordion

---
+++ components/InfoAccordion.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="join join-vertical mx-auto py-4">
+    <div class="collapse collapse-arrow join-item border-neutral border">
+      <input type="radio" name="my-accordion-4" checked="checked" >
+      <div class="collapse-title text-xl font-medium text-accent">Das ist EUDI</div>
+      <div class="collapse-content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+      </div>
+    </div>
+    <div class="collapse collapse-arrow join-item border-neutral border">
+      <input type="radio" name="my-accordion-4" >
+      <div class="collapse-title text-xl font-medium text-accent">Über den ZKP</div>
+      <div class="collapse-content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+      </div>
+    </div>
+    <div class="collapse collapse-arrow join-item border-neutral border">
+      <input type="radio" name="my-accordion-4" >
+      <div class="collapse-title text-xl font-medium text-accent">Über TICE</div>
+      <div class="collapse-content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
