78b5f3a824cd3afdc6b71b48c2a9fc65c6370adf
Tim Schauder <temscha@live.com>
2024-07-17 17:35:17 +0200

    Add start info to component

---
+++ components/StartInfo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="text-xl lg:text-3xl font-bold text-accent m-6">
+    <h1>Privacy by Design</h1>
+  </div>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+    magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat.</p>
+  <div class="my-12">
+    <NuxtLink to="/provider">
+      <button class="btn btn-primary">Jetzt testen</button>
+    </NuxtLink>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,9 @@
-
 <template>
   <div class="flex flex-col justify-center items-center">
-    <div class="text-xl lg:text-3xl font-bold text-accent m-6">
-      <h1>Privacy by Design</h1>
-    </div>
-    <button class="btn btn-primary">Jetzt testen</button>
+    <StartInfo/>
+    <InfoAccordion/>
   </div>
 </template>
 <script setup lang="ts">
+import StartInfo from "~/components/StartInfo.vue";
 </script>
